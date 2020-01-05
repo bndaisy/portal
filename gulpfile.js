65a47744af823dcec251419e63ca247b32ada62e
Анна <anna@MacBook-Pro-Anna.local>
2020-01-05 21:18:25 +0300

    Add Palette & Variables

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+// Main
+const gulp = require('gulp'),
+      del = require('del'),
+      browserSync = require('browser-sync').create();
+
+// CSS
+const sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      cleanCSS = require('gulp-clean-css');
+
+// HTML
+const pug = require('gulp-pug');
+
+//JavaScript
+const uglify = require('gulp-uglify');
+
+
+
+//Functions
+
+function styles(){
+    return gulp
+      .src("./public/sass/main.sass")
+      .pipe(
+        sass({
+          outputStyle: "expanded"
+        }).on("error", sass.logError)
+      )
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ["last 15 versions"],
+          cascade: false
+        })
+      )
+      .pipe(
+        cleanCSS({
+          compatibility: "ie8",
+          level: 2
+        })
+      )
+      .pipe(gulp.dest("./build/css"))
+      .pipe(browserSync.stream());
+                
+}
+
+function scripts(){
+    return gulp.src('./public/js/**/*.js')
+                .pipe(uglify({
+                    topLevel: true
+                }))
+                
+                .pipe(gulp.dest('./build/js'))
+}
+
+function html(){
+    return gulp
+      .src("./public/pug/**/*.pug")
+      .pipe(
+        pug({
+          pretty: "\t"
+        })
+      )
+      .pipe(gulp.dest("./build"))
+      .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
+    gulp.watch('./public/sass/**/*.sass', styles);
+    gulp.watch('./public/js/**/*.js', scripts);
+    gulp.watch('./public/pug/**/*.pug', html);
+    gulp.watch('.build/**/*.html', browserSync.reload);
+}
+
+function clean(){
+    del(['build/*']);
+}
+
+// Tasks
+
+gulp.task('watch', watch);
+
+gulp.task('build', gulp.series(clean,
+                    gulp.parallel(styles, scripts, html)
+                    ));
+
+gulp.task('dev', gulp.series('build', 'watch'));
