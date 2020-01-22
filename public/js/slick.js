ecadcd8130f5ebde67a1a5f82c385fbcb15a784c
Annie <daisy.brkn@gmail.com>
2020-01-22 21:10:41 +0300

    Create filter for ourteam page

---
+++ public/js/slick.js
@@ -0,0 +1,12 @@
+$('.new-collegue__items').slick({
+    prevArrow: $('.new-collegue .btn--slick-prev'),
+    nextArrow: $('.new-collegue .btn--slick-next')
+});
+$('.best-collegue__items').slick({
+    prevArrow: $('.best-collegue .btn--slick-prev'),
+    nextArrow: $('.best-collegue .btn--slick-next')
+});
+$('.birth-collegue__items').slick({
+    prevArrow: $('.birth-collegue .btn--slick-prev'),
+    nextArrow: $('.birth-collegue .btn--slick-next')
+});
