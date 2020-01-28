f9362e5cd923af39d99c907caf25574e46232012
Annie <daisy.brkn@gmail.com>
2020-01-28 15:59:54 +0300

    Correct align for Auth & PopUp

--- public/js/userPopUp.js
+++ public/js/userPopUp.js
@@ -2,21 +2,25 @@
     let card = document.querySelectorAll('.user-card'),
         popUp = document.querySelector('.user-popup'),
         btnClose = document.querySelector('.btn--close'),
-        substrate = document.querySelector(' .user-popup__substrate');
+        substrate = document.querySelector(' .user-popup__substrate'),
+        body = document.querySelector('body');
 
     card.forEach(item => item.addEventListener('click', () => {
         popUp.classList.add('is-active');
         substrate.classList.add('is-active');
+        body.classList.add('is-disabled');
     }));
 
     btnClose.addEventListener('click', () => {
         popUp.classList.remove('is-active');
         substrate.classList.remove('is-active');
+        body.classList.remove('is-disabled');
     });
 
     substrate.addEventListener('click', () => {
         popUp.classList.remove('is-active');
         substrate.classList.remove('is-active');
+        body.classList.remove('is-disabled');
     })
     
 })();
