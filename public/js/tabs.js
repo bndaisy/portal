63f0146f9bd80935ab15e9ae3cf00869cd5c326d
Annie <daisy.brkn@gmail.com>
2020-02-01 07:45:33 +0300

    Correct tabs script & add resp for tabs knowlege

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -2,11 +2,12 @@
     let tabsNavItems = document.querySelectorAll('.tabs-nav__item'),
         tabsContentItems = document.querySelectorAll('.tabs-content__item');
 
+    let clearClasses = (item) => item.classList.remove('is-active');
+
         function makeTabActive(event){
             let target = event.target.getAttribute('data-tab-name');
 
-            tabsNavItems.forEach(item => {
-                if(item.classList.contains('is-active')) item.classList.remove('is-active')});
+            tabsNavItems.forEach(item => clearClasses(item));
 
             this.classList.add('is-active');
 
@@ -15,7 +16,7 @@
 
         function makeContentActive(target){
             tabsContentItems.forEach(item => {
-                if(item.classList.contains('is-active')) item.classList.remove('is-active')
+                clearClasses(item)
                 if(item.classList.contains(target)) item.classList.add('is-active')
             });
         }
