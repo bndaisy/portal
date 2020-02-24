82a867661c53f062928b6244bb31d3cc08fde00d
Annie <daisy.brkn@gmail.com>
2020-02-24 18:01:11 +0300

    Responsive for news-page

--- public/js/news-slick.js
+++ public/js/news-slick.js
@@ -12,5 +12,29 @@ $('.slider__body').slick({
     dots: false,
     arrows: false,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
